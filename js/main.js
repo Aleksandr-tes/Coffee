0cc513cd8c78743b520b2ad2e0ed5c40477249d5
Aleksandr-tes <91590581+Aleksandr-tes@users.noreply.github.com>
2021-10-13 10:40:04 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -29,3 +29,45 @@ function f2() {
 burger.addEventListener('click', () => {
    burger.classList.toggle('burger-active');
 })
+
+
+// =====  jQuery =====
+// =====  jQuery =====
+// =====  jQuery =====
+
+
+$('.slyder-block').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
